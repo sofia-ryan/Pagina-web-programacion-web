dff302eade5b811813884b8612559054cd140384
sofia-ryan <137908092+sofia-ryan@users.noreply.github.com>
2023-11-13 20:48:56 -0300

    cambios-de-prueba

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function enviarMensaje() {
   alert ('Su mensaje se ha enviado con éxito. ');
 }
+function cambiarPaginaTestimonios(){
+  alert ('Ha entrado a la pagina de Testimonios!')
+}
+function cambiarPaginaPreguntas(){
+  alert ('Encuentre aqui las Preguntas Frecuentes!')
+}
+function cambiarPaginaDocumentacion(){
+  alert ('Encuentre aqui la Documentacion!')
+}
+function cambiarPaginaHome(){
+  alert ('Ha vuelto a la pagina principal.')
+}
+function cambiarPaginaContacto(){
+  alert ('Aqui puede contactarse con nosotros!')
+}
